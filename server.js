b683576e16efa0efc7b9f790aab9c8570d4f0be9
Yale Yuen <yaleyyuen@gmail.com>
2016-05-24 10:09:23 -0700

    finish step 1

--- server.js
+++ server.js
@@ -3,3 +3,23 @@
 // it should then send back jsonData on a GET to /data
 
 var jsonData = {count: 12, message: 'hey'};
+
+const express =  require('express');
+const fs = require('fs');
+
+let app = express();
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/index.html', (err) => {
+    res.status(500).send(err);
+  });
+});
+
+app.get('/data', (req,res) => {
+  res.send(jsonData);
+});
+
+app.listen(3000, () => {
+  console.log('listening on 3000')
+});
+
