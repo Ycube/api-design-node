5497c51399ba3f5789e861cc92dca35560cb4fc2
Yale Yuen <yaleyyuen@gmail.com>
2016-05-31 18:46:44 -0700

    finish step 6

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var api = require('./api/api');
-
+var error = require('./middleware/error');
 // setup the app middlware
 require('./middleware/appMiddlware')(app);
 
@@ -9,5 +9,6 @@ require('./middleware/appMiddlware')(app);
 app.use('/api/', api);
 // set up global error handling
 
+app.use(error);
 // export the app for testing
 module.exports = app;
