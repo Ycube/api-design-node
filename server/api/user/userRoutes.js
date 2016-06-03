fe85b2f60466f5296e27467eba3552b229b9e253
Yale Yuen <yaleyyuen@gmail.com>
2016-06-03 14:25:07 -0700

    finish step 10

--- server/api/user/userRoutes.js
+++ server/api/user/userRoutes.js
@@ -1,12 +1,7 @@
 var router = require('express').Router();
 var logger = require('../../util/logger');
-
-// setup boilerplate route jsut to satisfy a request
-// for building
-router.param('id', function(){});
-
-router.route('/')
-
-router.route('/:id')
+var controller = require('./userController');
+var createRoutes = require('../../utils/createRoutes');
+createRoutes(controller, router);
 
 module.exports = router;
